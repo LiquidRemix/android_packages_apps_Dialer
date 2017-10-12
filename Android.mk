59d935542e606c5e479e2cd99ff915e6605dd01f
Danny Baumann <dannybaumann@web.de>
2017-10-12 13:00:32 +0200

    Allow using private framework API.
    Change-Id: I2ae8882a3ca9d99ecba6e1b4f8d6ed426036818d

--- Android.mk
+++ Android.mk
@@ -298,9 +298,10 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 	dialer-mime4j-core-target \
 	dialer-mime4j-dom-target \
 	jsr305 \
+	legacy-test \
 	libphonenumber \
 	okhttp \
-	volley
+	volley \
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
 	android-support-design \
@@ -354,7 +355,6 @@ endif
 
 # End Bug: 37077388
 
-#LOCAL_SDK_VERSION := system_current
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := Dialer
 LOCAL_CERTIFICATE := shared
